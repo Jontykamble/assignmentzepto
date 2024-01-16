437538016cb591a11527f7f66508bcd183c54417
Jonty_jk <88649711+Jontykamble@users.noreply.github.com>
2024-01-16 13:24:47 +0530

    Update MainContainer.jsx

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -6,8 +6,8 @@ const MainContainer = () => {
     <div className="h-96 w-4/6 bg-neutral-100 p-4 border-b-[3px] border-neutral-300 relative">
       <InputBox />
       <div className="absolute bottom-0 text-lg text-neutral-500 font-semibold w-full flex justify-between pr-8 p-2">
-        <p>JAYASIMMAN P K</p>
-        <p>jayasi58460@gmail.com</p>
+
+        <p>jontykambleofficial@gmail.com</p>
       </div>
     </div>
   );
